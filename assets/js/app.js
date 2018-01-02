ff8a8ca6e36e4ca76f5a6bd04fa34708d5660ebc
RoxanaCS <roxale.cares@gmail.com>
2018-01-02 17:12:12 -0300

    agregando firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//para usar firebase se pega este código
+var config = {
+   apiKey: "AIzaSyAqRUe-CuzmexneLSTEZbeufdbQsfDiFqk",
+   authDomain: "red-social-f9af1.firebaseapp.com",
+   databaseURL: "https://red-social-f9af1.firebaseio.com",
+   projectId: "red-social-f9af1",
+   storageBucket: "red-social-f9af1.appspot.com",
+   messagingSenderId: "157507274842"
+ };
+ firebase.initializeApp(config);
